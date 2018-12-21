7054d36711cca3132fd89ed206d5f318bd9980c2
azorac <45796629+azorac@users.noreply.github.com>
2018-12-21 18:33:34 +0100

    Create Setup-KVM.sh

---
+++ Setup-KVM.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#  curl -L  https://github.com/azorac/sandstorm/raw/master/Setup-KVM.sh | bash
+#Install standard utilities
+sudo yum -y install nano net-tools git
+
+#Clone openstack-lab
+mkdir sandstorm
+git clone git://github.com/azorac/sandstorm sandstorm --branch master
+
+#Install KVM/QEMU
+sudo yum -y install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install
+
+#Install and configure network bridge utilities to enable VM to communicate to the rest of the world
+sudo yum -y install bridge-utils
+sudo usermod -aG libvirt $USER #add installing user to the libvirt group
+
+#Configure network
+sudo echo 'BRIDGE=br0' >> /etc/sysconfig/network-scripts/ifcfg-eno1
+sudo mv sandstorm/ifcfg-br0 /etc/sysconfig/network-scripts/
+
+##
+#Create VM folder
+##
+sudo mkdir /vm
+sudo mkdir /vm/iso
+sudo mkdir /vm/disk
+
+##
+#set VM folder ACL
+##
+sudo setfacl -m g:libvirt:r /vm
+sudo setfacl -m g:libvirt:rx /vm/iso
+sudo setfacl -m g:libvirt:rwx /vm/disk
+
+#Set autostart and Start libvirtd
+sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
+
+##
+# Download CentOS iso
+##
+curl http://ftp.ember.se/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso > centos.iso
+sudo mv centos.iso /vm/iso
+
+sudo reboot

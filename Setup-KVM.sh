94dc0d73cdf8769439a060a21712ba4b8221ce04
azorac <45796629+azorac@users.noreply.github.com>
2020-06-13 10:09:19 +0200

    Update Setup-KVM.sh

--- Setup-KVM.sh
+++ Setup-KVM.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #  curl -L  https://github.com/azorac/sandstorm/raw/master/Setup-KVM.sh | bash
 #Install standard utilities
-sudo yum -y install nano net-tools git
+sudo dnf -y install nano net-tools git
 
 #Clone openstack-lab
 mkdir sandstorm
 git clone git://github.com/azorac/sandstorm sandstorm --branch master
 
 #Install KVM/QEMU
-sudo yum -y install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install
+sudo dnf -y install qemu-kvm libvirt libguestfs-tools virt-install
 
 #Install and configure network bridge utilities to enable VM to communicate to the rest of the world
-sudo yum -y install bridge-utils
+sudo dnf -y install bridge-utils
 sudo usermod -aG libvirt $USER #add installing user to the libvirt group
 
 #Configure network
